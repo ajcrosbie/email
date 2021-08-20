f1f3eeb7ecc760a979d616165a09262825b542a7
ajcrosbie <aaron.j.crosbie@gmail.com>
2021-08-20 21:40:24 +0100

    small improvements made

--- processEmail.py
+++ processEmail.py
@@ -1,37 +1,43 @@
 import ReadEmail
 import writeEmail
+import string
 
 def check(recived, file, loginDetails):
     message = []
     with open(file, "r") as f:
         for i in f:
             c = i.split(";") 
-            for v in recived["body"].split():
+            s = recived["body"]
+            for char in string.punctuation:
+                s = s.replace(char, ' ')# remove punctuation 
+            for v in s.split():#
                 if c[0] == v:
-                    message.append(c[1])
+                    if not (c[1] in message):
+                        message.append(c[0] + ": "+ c[1])
                     
     if len(message) == 0:
         writeEmail.send_mail("I found nothing that help, sorry", "automatic response", loginDetails[2],
         [recived["from"]], loginDetails[0], loginDetails[1]) # if no keywords were found
     
     else:
-        m1 = "I found this that might help" 
+        m1 = "I found this that might help " 
         for i in message:
             m1 = m1 + i
         writeEmail.send_mail(m1, "automatic response", loginDetails[2],             
         [recived["from"]], loginDetails[0], loginDetails[1]) # can process multiple requests 
 
+
 def main():
     username = "experiencework4@gmail.com" # change and use environment variables
     password = "work.experience1" # change and use environment variables
     emailName = "Work Experience <experiencework4@gmail.com>" # change and use environment variables
-    file = "keywords.txt" # can change file name
+    file = "keywords.txt"  # change and use environment variables
     loginDetails = [username, password, emailName] 
 
 
     d = ReadEmail.searchMail(username, password) # login
     con = True
-    
+
     while con:
         f = next(d)
         if f == "stop":
