eaddc58394eb7e202ed4c2d6372fc83bd355c803
ajcrosbie <aaron.j.crosbie@gmail.com>
2021-08-25 17:19:35 +0100

    reduced failure rates

--- test_email.py
+++ test_email.py
@@ -6,18 +6,42 @@ def test_SendRead():
     username = "experiencework4@gmail.com" # change and use environment variables
     password = "work.experience1" # change and use environment variables
     emailName = "Work Experience <experiencework4@gmail.com>" # change and use environment variables
-
+    
+    
+    temp = True
+    t = ReadEmail.searchMail(username, password) 
+    while temp:
+        c = next(t)
+        if c == "stop":
+            temp = False
+    # only test if no unread emails are there but this will clear them
+    
+    
     writeEmail.send_mail("dog", "none", emailName, [username], username, password) # change 1st argument
     f = ReadEmail.searchMail(username, password)
-    v = next(f)["body"].split()
+    
+    v = []
+    temp = True
+    while temp:
+        c = next(f)
+        if c == "stop":
+            temp = False
+        else:
+            v.append(i for i in c.split())
     assert "dog" in v # change to keyword from actual keywords file
 
 def test_process():
     username = "experiencework4@gmail.com" # change and use environment variables
     password = "work.experience1" # change and use environment variables
     emailName = "Work Experience <experiencework4@gmail.com>" # change and use environment variables
-    file = "keywords.txt"  # change and use environment variables
-    processEmail.main()
+    # change and use environment variables
+    temp = True
+    t = ReadEmail.searchMail(username, password) 
+    while temp:
+        c = next(t)
+        if c == "stop":
+            temp = False
+    
     writeEmail.send_mail("dog", "none", emailName, [username], username, password)# change 1st argument
     processEmail.main()
     f = ReadEmail.searchMail(username, password)
