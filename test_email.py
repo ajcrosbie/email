178441da74b01b42a336b4c78c43c19f04570d7d
ajcrosbie <aaron.j.crosbie@gmail.com>
2021-08-25 14:52:22 +0100

    small change

--- test_email.py
+++ test_email.py
@@ -7,11 +7,10 @@ def test_SendRead():
     password = "work.experience1" # change and use environment variables
     emailName = "Work Experience <experiencework4@gmail.com>" # change and use environment variables
 
-    writeEmail.send_mail("dog", "none", emailName, [username], username, password)
+    writeEmail.send_mail("dog", "none", emailName, [username], username, password) # change 1st argument
     f = ReadEmail.searchMail(username, password)
     v = next(f)["body"].split()
-    print(v)
-    assert "dog" in v
+    assert "dog" in v # change to keyword from actual keywords file
 
 def test_process():
     username = "experiencework4@gmail.com" # change and use environment variables
@@ -19,7 +18,7 @@ def test_process():
     emailName = "Work Experience <experiencework4@gmail.com>" # change and use environment variables
     file = "keywords.txt"  # change and use environment variables
     processEmail.main()
-    writeEmail.send_mail("dog", "none", emailName, [username], username, password)
+    writeEmail.send_mail("dog", "none", emailName, [username], username, password)# change 1st argument
     processEmail.main()
     f = ReadEmail.searchMail(username, password)
     assert "best" in next(f)["body"].split()
